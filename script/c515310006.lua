e3fd01893e1eec8d735290d493786c8d00bb494d
AlphaKretin <deewhydeeecks@gmail.com>
2017-08-24 16:18:23 +0930

    Fix IDs in script

--- script/c515310006.lua
+++ script/c515310006.lua
@@ -1,6 +1,6 @@
 --Elemelon-tal Farmistress Doriado
 --AlphaKretin
-function c5150310006.initial_effect(c)
+function c515310006.initial_effect(c)
 	--Attribute
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -14,8 +14,8 @@ function c5150310006.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetTarget(c5150310006.target)
-	e2:SetOperation(c5150310006.operation)
+	e2:SetTarget(c515310006.target)
+	e2:SetOperation(c515310006.operation)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -24,13 +24,13 @@ function c5150310006.initial_effect(c)
 	e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e4)
 end
-function c5150310006.cfilter(c)
+function c515310006.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xf31)
 end
-function c5150310006.filter(c)
+function c515310006.filter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
-function c5150310006.ccount(g)
+function c515310006.ccount(g)
 	local att=0
 	for tc in aux.Next(g) do
 		if tc then att=bit.bor(att,tc:GetAttribute()) end
@@ -42,28 +42,28 @@ function c5150310006.ccount(g)
 	end
 	return ct
 end
-function c5150310006.thfilter(c)
+function c515310006.thfilter(c)
 	return c:IsRace(RACE_PLANT) and c:IsAttribute(ATTRIBUTE_EARTH+ATTRIBUTE_WATER+ATTRIBUTE_FIRE+ATTRIBUTE_WIND) and c:IsAbleToHand()
 end
-function c5150310006.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c515310006.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local cg=Duel.GetMatchingGroup(c5150310006.cfilter,tp,LOCATION_MZONE,0,c)
+	local cg=Duel.GetMatchingGroup(c515310006.cfilter,tp,LOCATION_MZONE,0,c)
 	cg:KeepAlive()
-	local ct=c5150310006.ccount(cg)
-	local dt=Duel.GetMatchingGroupCount(c5150310006.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	local ct=c515310006.ccount(cg)
+	local dt=Duel.GetMatchingGroupCount(c515310006.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	local b1=dt>=ct
-	local b2=Duel.IsExistingMatchingCard(c5150310006.thfilter,tp,LOCATION_DECK,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(c515310006.thfilter,tp,LOCATION_DECK,0,1,nil)
 	if chk==0 then return b1 or b2 end
 	local off=1
 	local ops={}
 	local opval={}
 	if b1 then
-		ops[off]=aux.Stringid(5150310006,0)
+		ops[off]=aux.Stringid(515310006,0)
 		opval[off-1]=1
 		off=off+1
 	end
 	if b2 then
-		ops[off]=aux.Stringid(5150310006,1)
+		ops[off]=aux.Stringid(515310006,1)
 		opval[off-1]=2
 		off=off+1
 	end
@@ -72,27 +72,27 @@ function c5150310006.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(sel)
 	if sel==1 then
 		e:SetCategory(CATEGORY_DESTROY)
-		local g=Duel.GetMatchingGroup(c5150310006.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		local g=Duel.GetMatchingGroup(c515310006.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,ct,0,LOCATION_SZONE)
 	elseif sel==2 then
 		e:SetCategory(CATEGORY_TOHAND)
-		local g=Duel.GetMatchingGroup(c5150310006.thfilter,tp,LOCATION_DECK,0,nil)
+		local g=Duel.GetMatchingGroup(c515310006.thfilter,tp,LOCATION_DECK,0,nil)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 	end
 end
-function c5150310006.operation(e,tp,eg,ep,ev,re,r,rp)
+function c515310006.operation(e,tp,eg,ep,ev,re,r,rp)
 	local sel=e:GetLabel()
 	local c=e:GetHandler()
 	if sel==1 then
-		local cg=Duel.GetMatchingGroup(c5150310006.cfilter,tp,LOCATION_MZONE,0,c)
+		local cg=Duel.GetMatchingGroup(c515310006.cfilter,tp,LOCATION_MZONE,0,c)
 		cg:KeepAlive()
-		local ct=c5150310006.ccount(cg)
+		local ct=c515310006.ccount(cg)
 		if ct>0 then
-			local g=Duel.SelectMatchingCard(tp,c5150310006.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
+			local g=Duel.SelectMatchingCard(tp,c515310006.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
 			Duel.Destroy(g,REASON_EFFECT)
 		end
 	elseif sel==2 then
-		local g=Duel.SelectMatchingCard(tp,c5150310006.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c515310006.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
