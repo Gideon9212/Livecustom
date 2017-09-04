7700234c2d537d42786303abb6c4d583cfb5025a
AlphaKretin <deewhydeeecks@gmail.com>
2017-09-04 14:36:39 +0930

    Fix "Beefstritium Isotomato"

--- script/c515310104.lua
+++ script/c515310104.lua
@@ -47,7 +47,7 @@ function c515310104.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c515310104.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c515310104.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c515310104.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
