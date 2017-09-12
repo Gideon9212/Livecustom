890db41e1008d3af766b3a405b01d8bb0c30bd9c
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-12 16:13:18 -0300

    Update c515242591.lua

--- script/c515242591.lua
+++ script/c515242591.lua
@@ -1,9 +1,9 @@
 --Blue Striker: Tiny Pony, the Wise
 function c515242591.initial_effect(c)
-		--link summon
-aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x666),2)
+	--link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x666),2)
 	c:EnableReviveLimit()
-			--search if deal damage
+	--search if deal damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(4066,5))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -14,7 +14,7 @@ aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x666),2)
 	e1:SetTarget(c515242591.sptg)
 	e1:SetOperation(c515242591.spop)
 	c:RegisterEffect(e1)
-			--search if deal damage
+	--search if deal damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(4066,6))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -25,7 +25,7 @@ aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x666),2)
 	e2:SetTarget(c515242591.sptg2)
 	e2:SetOperation(c515242591.spop2)
 	c:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(c)
+	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_GRAVE)
@@ -48,7 +48,7 @@ function c515242591.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
-	Duel.SendtoGrave(tc,nil,2,REASON_EFFECT)
+		Duel.SendtoGrave(tc,nil,2,REASON_EFFECT)
 	end
 end
 function c515242591.atkcon2(e,tp,eg,ep,ev,re,r,rp)
@@ -60,11 +60,10 @@ end
 function c515242591.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() end
 	if chk==0 then return Duel.IsExistingMatchingCard(c515242591.filter2,tp,LOCATION_GRAVE,0,1,nil) end
-	
 	Duel.SetOperationInfo(0,CATEGORY_HINTMSG_ATOEXTRA,g,1,0,0)
 end
 function c515242591.spop2(e,tp,eg,ep,ev,re,r,rp)
-local hg=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
+	local hg=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOEXTRA)
 	local g=Duel.SelectMatchingCard(tp,c515242591.filter2,tp,LOCATION_GRAVE,0,1,2,nil)
 	if g:GetCount()>0 then
@@ -72,7 +71,7 @@ local hg=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
 	end
 end
 function c515242591.thfilter(c)
-	return c:IsType(TYPE_SPELL) c:IsSetCard(0x666) and c:IsAbleToHand()
+	return c:IsType(TYPE_SPELL) and c:IsSetCard(0x666) and c:IsAbleToHand()
 end
 function c515242591.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c515242591.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
