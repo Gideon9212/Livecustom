4dc77bc6d7a492f463f163772938449c8cf8a34a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-24 21:50:00 -0300

    fix
    thing's

--- script/c515004000.lua
+++ script/c515004000.lua
@@ -107,27 +107,23 @@ function c515004000.initial_effect(c)
 	end
 end
 function c515004000.fil(...)
-	local c,val,sc,sumtype,tp,g
-	local t={...}
-    	if type(t[2])=='boolean' then
-        	tp,g=t[3],t[4]
-        	return g:IsExists(Card.IsType,1,nil,TYPE_TUNER) and g:IsExists(aux.NOT(Card.IsType),1,nil,TYPE_TUNER)
-    	elseif type(t[3])=='number' then
+    	local c,val,sc,sumtype,tp,g
+    	local t={...}
+    	if type(t[3])=='number' then
         	c,sc,sumtype,tp=table.unpack(t)
     	else
         	c,val,sc,sumtype,tp=table.unpack(t)
     	end
-	return c:IsLevel(6) and c:IsLocation(LOCATION_ONFIELD) and c:IsRace(RACE_DRAGON,sc,sumtype,tp)
+    	return c:IsLevel(6) and c:IsLocation(LOCATION_ONFIELD) and c:IsRace(RACE_DRAGON,sc,sumtype,tp)
 end
 function c515004000.fil2(c,sc,sumtype,tp)
 	return c:IsLevel(6) and c:IsLocation(LOCATION_ONFIELD) and c:IsRace(RACE_DRAGON,sc,sumtype,tp) and c:IsType(TYPE_TUNER,sc,sumtype,tp)
 end
 function c515004000.xyzcheck(g,tp,xyz)
-    return g:IsExists(c515004000.xyzchk,1,nil,xyz,SUMMON_TYPE_XYZ,tp)
+    	return g:IsExists(c515004000.xyzchk,1,nil,xyz,SUMMON_TYPE_XYZ,tp,g)
 end
-function c515004000.xyzchk(c,g,tp,xyz)
-    return c:IsType(TYPE_TUNER,xyz,SUMMON_TYPE_XYZ,tp) and 
-        g:IsExists(aux.NOT(Card.IsType),1,c,TYPE_TUNER,xyz,SUMMON_TYPE_XYZ,tp)
+function c515004000.xyzchk(c,xyz,sumtype,tp,g)
+    	return c:IsType(TYPE_TUNER,xyz,SUMMON_TYPE_XYZ,tp) and g:IsExists(aux.NOT(Card.IsType),1,c,TYPE_TUNER,xyz,SUMMON_TYPE_XYZ,tp)
 end
 function c515004000.sumop(e,tp,eg,ep,ev,re,r,rp)
 	for tc in aux.Next(eg) do
