fe0d9cd7d96f3471d80bdbdecc6c50c5a2023c38
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-29 02:51:57 -0300

    Update c515220020.lua

--- script/c515220020.lua
+++ script/c515220020.lua
@@ -96,7 +96,7 @@ function c515220020.pcop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local g=Duel.SelectMatchingCard(tp,c515220020.pcfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
-		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
 function c515220020.splimit(e,c,sump,sumtype,sumpos,targetp,se)
