4a803f7a1a08b8f5f95889325a99a6773419f495
larry126 <larry126@users.noreply.github.com>
2017-10-04 16:43:56 +0100

    Updated various parts of the script

--- script/c515002502.lua
+++ script/c515002502.lua
@@ -35,7 +35,7 @@ function c515002502.initial_effect(c)
 	e3:SetOperation(c515002502.operation2)
 	c:RegisterEffect(e3) 
 end
-c515002502.listed_names={46986414,515002502,38033121,30208479}
+c515002502.card_code_list={46986414,515002502,38033121,30208479}
 function c515002502.condition(e,tp,eg,ep,ev,re,r,rp)
 	local tn=Duel.GetTurnPlayer()
 	local ph=Duel.GetCurrentPhase()
@@ -135,98 +135,3 @@ function c515002502.operation(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
---[[function c515002502.operation(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<1 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(515002502,0))
-	local g=Duel.SelectMatchingCard(tp,c515002502.filter,tp,LOCATION_DECK,0,1,1,nil,tp)
-	local tc=g:GetFirst()
-	local te=tc:GetActivateEffect()
-	if not te then return end
-	local pre={Duel.GetPlayerEffect(tp,EFFECT_CANNOT_ACTIVATE)}
-	if pre[1] then
-		for i,eff in ipairs(pre) do
-			local prev=eff:GetValue()
-			if type(prev)~='function' or prev(eff,te,tp) then return end
-		end
-	end
-	if (Duel.GetLocationCount(tp,LOCATION_SZONE)>0 or tc:IsType(TYPE_FIELD)) and tc:IsType(TYPE_SPELL+TYPE_TRAP) 
-		and tc:CheckActivateEffect(false,false,false)~=nil and not tc:IsHasEffect(EFFECT_CANNOT_TRIGGER) then
-		local tpe=tc:GetType()
-		local tg=te:GetTarget()
-		local co=te:GetCost()
-		local op=te:GetOperation()
-		e:SetCategory(te:GetCategory())
-		e:SetProperty(te:GetProperty())
-		Duel.ClearTargetCard()
-		if bit.band(tpe,TYPE_FIELD)~=0 then
-			local fc=Duel.GetFieldCard(1-tp,LOCATION_SZONE,5)
-			if Duel.IsDuelType(DUEL_OBSOLETE_RULING) then
-				if fc then Duel.Destroy(fc,REASON_RULE) end
-				fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
-				if fc and Duel.Destroy(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
-			else
-				fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
-				if fc and Duel.SendtoGrave(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
-			end
-		end
-		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		Duel.Hint(HINT_CARD,0,tc:GetOriginalCode())
-		tc:CreateEffectRelation(te)
-		if bit.band(tpe,TYPE_EQUIP+TYPE_CONTINUOUS+TYPE_FIELD)==0 and not tc:IsHasEffect(EFFECT_REMAIN_FIELD) then
-			tc:CancelToGrave(false)
-		end
-		if te:GetCode()==EVENT_CHAINING then
-			local te2=Duel.GetChainInfo(chain,CHAININFO_TRIGGERING_EFFECT)
-			local tc=te2:GetHandler()
-			local g=Group.FromCards(tc)
-			local p=tc:GetControler()
-			if co then co(te,tp,g,p,chain,te2,REASON_EFFECT,p,1) end
-			if tg then tg(te,tp,g,p,chain,te2,REASON_EFFECT,p,1) end
-		elseif te:GetCode()==EVENT_FREE_CHAIN then
-			if co then co(te,tp,eg,ep,ev,re,r,rp,1) end
-			if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
-		else
-			local res,teg,tep,tev,tre,tr,trp=Duel.CheckEvent(te:GetCode(),true)
-			if co then co(te,tp,teg,tep,tev,tre,tr,trp,1) end
-			if tg then tg(te,tp,teg,tep,tev,tre,tr,trp,1) end
-		end
-		Duel.BreakEffect()
-		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-		if g then
-			local etc=g:GetFirst()
-			while etc do
-				etc:CreateEffectRelation(te)
-				etc=g:GetNext()
-			end
-		end
-		tc:SetStatus(STATUS_ACTIVATED,true)
-		if not tc:IsDisabled() then
-			if te:GetCode()==EVENT_CHAINING then
-				local te2=Duel.GetChainInfo(chain,CHAININFO_TRIGGERING_EFFECT)
-				local tc=te2:GetHandler()
-				local g=Group.FromCards(tc)
-				local p=tc:GetControler()
-				if op then op(te,tp,g,p,chain,te2,REASON_EFFECT,p) end
-			elseif te:GetCode()==EVENT_FREE_CHAIN then
-				if op then op(te,tp,eg,ep,ev,re,r,rp) end
-			else
-				local res,teg,tep,tev,tre,tr,trp=Duel.CheckEvent(te:GetCode(),true)
-				if op then op(te,tp,teg,tep,tev,tre,tr,trp) end
-			end
-		else
-			--insert negated animation here
-		end
-		Duel.RaiseEvent(Group.CreateGroup(tc),EVENT_CHAIN_SOLVED,te,0,tp,tp,Duel.GetCurrentChain())
-		if g and tc:IsType(TYPE_EQUIP) and not tc:GetEquipTarget() then
-			Duel.Equip(tp,tc,g:GetFirst())
-		end
-		tc:ReleaseEffectRelation(te)
-		if etc then 
-			etc=g:GetFirst()
-			while etc do
-				etc:ReleaseEffectRelation(te)
-				etc=g:GetNext()
-			end
-		end
-	end
-end]]
