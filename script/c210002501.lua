1c3592d4d264c905a730eea0162759cff3989e9d
larry126 <larry126@users.noreply.github.com>
2017-10-16 19:09:34 +0100

    Errata'd Dark Magic Screen

--- script/c210002501.lua
+++ script/c210002501.lua
@@ -1,5 +1,5 @@
---Dark Magic Screen
---scripted by Larry126
+--Dark Magic Aroma
+--designed and scripted by Larry126
 function c210002501.initial_effect(c)
     c:SetUniqueOnField(1,0,210002501)
     local e1=Effect.CreateEffect(c)
@@ -9,35 +9,30 @@ function c210002501.initial_effect(c)
     e1:SetCountLimit(1,210002501)
     e1:SetOperation(c210002501.activate)
     c:RegisterEffect(e1)
-    --
+    --disable
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_CHAIN_SOLVING)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e2:SetTargetRange(LOCATION_ONFIELD,0)
-    e2:SetCondition(c210002501.indcon)
-    e2:SetTarget(c210002501.indtg)
-    e2:SetValue(c210002501.indval)
+    e2:SetCondition(c210002501.negcon)
+    e2:SetOperation(c210002501.negop)
     c:RegisterEffect(e2)
-    local e3=e2:Clone()
-    e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e3:SetValue(aux.tgoval)
+    --atk boost
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_UPDATE_ATTACK)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetTargetRange(LOCATION_ONFIELD,0)
+    e3:SetTarget(c210002501.atktg)
+    e3:SetOperation(c210002501.atkval)
     c:RegisterEffect(e3)
+    local e4=e3:Clone()
+    e4:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e4)
 end
 c210002501.card_code_list={46986414,210002501}
-function c210002501.cfilter(c)
-    return c:IsFaceup() and c:IsCode(46986414)
-end
-function c210002501.indcon(e)
-    return Duel.IsExistingMatchingCard(c210002501.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
-end
-function c210002501.indtg(e,c)
-    return c:IsType(TYPE_CONTINUOUS) and aux.IsCodeListed(c,46986414)
-end
-function c210002501.indval(e,re,tp)
-    return tp~=e:GetHandlerPlayer()
-end
 function c210002501.filter(c)
     return (c:IsCode(46986414) or 
         (aux.IsCodeListed(c,46986414) and c:IsType(TYPE_SPELL+TYPE_TRAP)) or
@@ -47,10 +42,39 @@ end
 function c210002501.activate(e,tp,eg,ep,ev,re,r,rp)
     if not e:GetHandler():IsRelateToEffect(e) then return end
     local g=Duel.GetMatchingGroup(c210002501.filter,tp,LOCATION_DECK,0,nil)
-    if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(210002501,0)) then
+    if g:GetCount()>0 and Duel.SelectEffectYesNo(tp,e:GetHandler()) then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
         local sg=g:Select(tp,1,1,nil)
         Duel.SendtoHand(sg,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,sg)
     end
+end
+function c210002501.cfilter(c)
+    return c:IsFaceup() and c:IsCode(46986414)
+end
+function c210002501.tfilter(c,tp)
+    return c:IsFaceup() and c:IsType(TYPE_CONTINUOUS) and aux.IsCodeListed(c,46986414)
+        and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+end
+function c210002501.negcon(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)    
+    return e:GetHandler():GetFlagEffect(210002501)==0
+        and Duel.IsExistingMatchingCard(c210002501.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+        and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
+        and g and g:IsExists(c210002501.tfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function c210002501.negop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.SelectEffectYesNo(tp,e:GetHandler()) then
+        e:GetHandler():RegisterFlagEffect(210002501,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+        Duel.NegateEffect(ev)
+    end
+end
+function c210002501.atktg(e,c)
+    return c:IsFaceup() and (c:IsSetCard(0x10a2) or (c:IsRace(RACE_SPELLCASTER) and (c:IsLevelAbove(6) or c:IsRankAbove(6))))
+end
+function c210002501.atkfilter(c)
+    return c:IsFaceup() and c:IsType(TYPE_CONTINUOUS)
+end
+function c210002501.atkval(e,c)
+    return Duel.GetMatchingGroupCount(c210002501.atkfilter,tp,LOCATION_ONFIELD,0,nil)*100
 end
