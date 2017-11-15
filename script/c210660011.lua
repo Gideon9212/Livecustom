788f2df72c7f5c4110c295305d3770038a52ddef
Larry126 <larry126@users.noreply.github.com>
2017-11-15 22:12:22 +0000

    Updated effect script

--- script/c210660011.lua
+++ script/c210660011.lua
@@ -9,29 +9,29 @@ function c210660011.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SUMMON_PROC)
 	e1:SetCondition(c210660011.ntcon)
+	e1:SetOperation(c210660011.ntop)
 	c:RegisterEffect(e1)
-	--cannot normal summon
+	--draw
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_SUMMON_COST)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetCost(c210660011.sumcost)
-	e2:SetOperation(c210660011.sumop)
+	e2:SetDescription(aux.Stringid(1945387,0))
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCost(c210660011.drcost)
+	e2:SetTarget(c210660011.drtg)
+	e2:SetOperation(c210660011.drop)
 	c:RegisterEffect(e2)
-	--atk up
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(10789972,0))
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
-	e3:SetHintTiming(TIMING_DAMAGE_STEP+TIMING_END_PHASE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(c210660011.atkcon)
-	e3:SetTarget(c210660011.atktg)
-	e3:SetOperation(c210660011.atkop)
-	c:RegisterEffect(e3)
+	--atk
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_REPEAT+EFFECT_FLAG_DELAY)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(c210660011.adval)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_SET_DEFENSE_FINAL)
+	c:RegisterEffect(e5)
 end
 function c210660011.ntcon(e,c,minc)
 	if c==nil then return true end
@@ -39,38 +39,58 @@ function c210660011.ntcon(e,c,minc)
 		and Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0)==0
 		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
-function c210660011.sumcost(e,c,tp)
-	return Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)==0
-end
-function c210660011.sumop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
+function c210660011.ntop(e,tp,eg,ep,ev,re,r,rp,c)
+	if Duel.GetFlagEffect(tp,210660011)~=0 then return end
+	--extra summon
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_FIEND))
 	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetTargetRange(1,0)
 	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_EXTRA_SET_COUNT)
+	Duel.RegisterEffect(e2,tp)
+	Duel.RegisterFlagEffect(tp,210660011,RESET_PHASE+PHASE_END,0,1)
 end
-function c210660011.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+function c210660011.cfilter(c)
+	return c:IsLevelAbove(10) and c:IsRace(RACE_FIEND) and not c:IsPublic()
 end
-function c210660011.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,LOCATION_MZONE)
-end
-function c210660011.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
+function c210660011.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and c:IsRelateToEffect(e) and c:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		e1:SetValue(tc:GetAttack()+100)
-		c:RegisterEffect(e1)
+	if chk==0 then return not c:IsPublic()
+		and Duel.IsExistingMatchingCard(c210660011.cfilter,tp,LOCATION_HAND,0,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,c210660011.cfilter,tp,LOCATION_HAND,0,1,1,c)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+	e:SetLabelObject(g:GetFirst())
+end
+function c210660011.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function c210660011.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.SendtoDeck(Group.FromCards(e:GetHandler(),e:GetLabelObject()),nil,2,REASON_EFFECT)==2 then
+		Duel.ShuffleDeck(p)
+		Duel.BreakEffect()
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
+end
+function c210660011.filter(c)
+	return c:IsFaceup() and not c:IsCode(21208154,210660011)
+end
+function c210660011.adval(e,c)
+	local g=Duel.GetMatchingGroup(c210660011.filter,e:GetHandlerPlayer(),0,LOCATION_MZONE,nil)
+	if g:GetCount()==0 then 
+		return 100
+	else
+		local tg,val=g:GetMaxGroup(Card.GetAttack)
+		return val+100
 	end
 end
