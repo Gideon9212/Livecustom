c704232bf1bb75e9659bba917a5c6d191bb3760e
Larry126 <larry126@users.noreply.github.com>
2018-02-09 08:11:24 +0000

    New cards along with old cards errata'd and clean up

--- script/c210002501.lua
+++ script/c210002501.lua
@@ -1,77 +1,59 @@
 --Dark Magic Aurora
 --designed and scripted by Larry126
 function c210002501.initial_effect(c)
-    c:SetUniqueOnField(1,0,210002501)
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1,210002501)
-    e1:SetOperation(c210002501.activate)
-    c:RegisterEffect(e1)
-    --disable
-    local e2=Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_DISABLE)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_CHAIN_SOLVING)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetCondition(c210002501.negcon)
-    e2:SetOperation(c210002501.negop)
-    c:RegisterEffect(e2)
-    --atk boost
-    local e3=Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_ATKCHANGE)
-    e3:SetType(EFFECT_TYPE_FIELD)
-    e3:SetCode(EFFECT_UPDATE_ATTACK)
-    e3:SetRange(LOCATION_SZONE)
-    e3:SetTargetRange(LOCATION_MZONE,0)
-    e3:SetTarget(c210002501.atktg)
-    e3:SetValue(c210002501.atkval)
-    c:RegisterEffect(e3)
-    local e4=e3:Clone()
-    e4:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e4)
+	c:SetUniqueOnField(1,0,210002501)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,210002501)
+	e1:SetOperation(c210002501.activate)
+	c:RegisterEffect(e1)
+	--leave replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SEND_REPLACE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(c210002501.reptg)
+	e2:SetValue(c210002501.repval)
+	e2:SetOperation(c210002501.repop)
+	c:RegisterEffect(e2)
 end
 c210002501.listed_names={46986414,210002501}
 function c210002501.filter(c)
-    return (c:IsCode(46986414) or 
-        (aux.IsCodeListed(c,46986414) and c:IsType(TYPE_SPELL+TYPE_TRAP))
-        or (c:IsSetCard(0xa1) and c:IsType(TYPE_SPELL)))
-        and c:IsAbleToHand() and not c:IsCode(210002501)
+	return (c:IsCode(46986414)
+		or (c:IsSetCard(0xa1) and c:IsType(TYPE_SPELL)))
+		and c:IsAbleToHand() and not c:IsCode(210002501)
 end
 function c210002501.activate(e,tp,eg,ep,ev,re,r,rp)
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    local g=Duel.GetMatchingGroup(c210002501.filter,tp,LOCATION_DECK,0,nil)
-    if g:GetCount()>0 and Duel.SelectEffectYesNo(tp,e:GetHandler()) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.SendtoHand(sg,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,sg)
-    end
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(c210002501.filter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectEffectYesNo(tp,e:GetHandler()) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
 function c210002501.cfilter(c)
-    return c:IsFaceup() and c:IsCode(46986414)
+	return c:IsFaceup() and c:IsCode(46986414)
 end
 function c210002501.tfilter(c,tp)
-    return c:IsFaceup() and c:IsType(TYPE_CONTINUOUS) and aux.IsCodeListed(c,46986414)
-        and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+	return c:IsFaceup() and c:IsType(TYPE_CONTINUOUS) and aux.IsCodeListed(c,46986414)
+		and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT)
 end
-function c210002501.negcon(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)    
-    return e:GetHandler():GetFlagEffect(210002501)==0
-        and Duel.IsExistingMatchingCard(c210002501.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
-        and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
-        and g and g:IsExists(c210002501.tfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+function c210002501.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return eg:IsExists(c210002501.tfilter,1,nil,tp)
+		and Duel.GetFlagEffect(e:GetHandlerPlayer(),210002501)==0
+		and Duel.IsExistingMatchingCard(c210002501.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	return Duel.SelectEffectYesNo(tp,c,96)
 end
-function c210002501.negop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.SelectEffectYesNo(tp,e:GetHandler()) then
-        e:GetHandler():RegisterFlagEffect(210002501,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
-        Duel.NegateEffect(ev)
-    end
+function c210002501.repval(e,c)
+	return c210002501.tfilter(c,e:GetHandlerPlayer())
 end
-function c210002501.atktg(e,c)
-    return c:IsFaceup() and (c:IsSetCard(0x10a2) or (c:IsRace(RACE_SPELLCASTER) and (c:IsLevelAbove(6) or c:IsRankAbove(6))))
-end
-function c210002501.atkval(e,c)
-    return Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_SZONE,0,nil)*100
+function c210002501.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,210002501,RESET_PHASE+PHASE_END,0,1)
+	Duel.DiscardHand(tp,Card.IsAbleToGrave,1,1,REASON_EFFECT+REASON_REPLACE,nil)
 end
