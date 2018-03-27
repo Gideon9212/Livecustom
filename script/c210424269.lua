788af7f84f3e3c97e7e0dd22e18cf9c3a38f93f0
MoonBurst <Moonburstplays@gmail.com>
2018-03-26 19:26:23 -0500

    Add files via upload

--- script/c210424269.lua
+++ script/c210424269.lua
@@ -30,7 +30,7 @@ function card.lfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x666)
 end
 function card.filter2(c,tp)
-	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsSetCard(0x666)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD) and c:IsSetCard(0x666)
 end
 function card.betarget(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
