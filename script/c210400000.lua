dfcd7d2fff6eff405178f56c457a9f878e6cce4f
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-04-12 23:14:21 -0300

    Update c210400000.lua

--- script/c210400000.lua
+++ script/c210400000.lua
@@ -1,16 +1,15 @@
 --Mecha Phantom Beast Pegashin
-function cyyyyyy.initial_effect(c)
+function c210400000.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0x101b),2,2)
-	
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0x101b),2,2)	
 	--indes
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e1:SetCondition(cyyyyyy.indcon)
+	e1:SetCondition(c210400000.indcon)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
@@ -18,30 +17,30 @@ function cyyyyyy.initial_effect(c)
 	c:RegisterEffect(e2)
 	--spsummon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(yyyyyy,0))
+	e3:SetDescription(aux.Stringid(210400000,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCost(cyyyyyy.tkcost)
-	e3:SetTarget(cyyyyyy.tktg)
-	e3:SetOperation(cyyyyyy.tkop)
+	e3:SetCost(c210400000.tkcost)
+	e3:SetTarget(c210400000.tktg)
+	e3:SetOperation(c210400000.tkop)
 	c:RegisterEffect(e3)
 end
-function cyyyyyy.indcon(e)
+function c210400000.indcon(e)
 	return Duel.IsExistingMatchingCard(Card.IsType,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,TYPE_TOKEN)
 end
 
-function cyyyyyy.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c210400000.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
-function cyyyyyy.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c210400000.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
 		if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,31533705,0x101b,0x4011,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
 end
-function cyyyyyy.tkop(e,tp,eg,ep,ev,re,r,rp)
+function c210400000.tkop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	if Duel.IsPlayerCanSpecialSummonMonster(tp,31533705,0x101b,0x4011,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) then
 		local token=Duel.CreateToken(tp,72291079)
