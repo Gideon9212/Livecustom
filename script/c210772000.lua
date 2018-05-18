f1c8e20dd608cbfb674a94af01dc2846f45cf0f7
Julien <lrhel@users.noreply.github.com>
2018-05-19 00:01:55 +0200

    Fixing setcode of Kuribohel and Lore text of Call of critias (#213)

--- script/c210772000.lua
+++ script/c210772000.lua
@@ -1,5 +1,5 @@
 --Kuribohel
-function c270772000.initial_effect(c)
+function c210772000.initial_effect(c)
 	--battle
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -13,14 +13,14 @@ function c270772000.initial_effect(c)
 	c:RegisterEffect(e2)
 	--damage
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(270772000,0))
+	e3:SetDescription(aux.Stringid(210772000,0))
 	e3:SetCategory(CATEGORY_DAMAGE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_BATTLE_CONFIRM)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetCondition(c270772000.damcon)
-	e3:SetTarget(c270772000.damtg)
-	e3:SetOperation(c270772000.damop)
+	e3:SetCondition(c210772000.damcon)
+	e3:SetTarget(c210772000.damtg)
+	e3:SetOperation(c210772000.damop)
 	c:RegisterEffect(e3)
 	--discard
 	local e4=Effect.CreateEffect(c)
@@ -29,75 +29,75 @@ function c270772000.initial_effect(c)
 	e4:SetCode(EVENT_PHASE+PHASE_END)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
-	e4:SetCondition(c270772000.mtcon)
-	e4:SetOperation(c270772000.mtop)
+	e4:SetCondition(c210772000.mtcon)
+	e4:SetOperation(c210772000.mtop)
 	c:RegisterEffect(e4)
 	--special summon
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(270772000,3))
+	e5:SetDescription(aux.Stringid(210772000,3))
 	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e5:SetCode(EVENT_LEAVE_FIELD)
-	e5:SetCondition(c270772000.spcon)
-	e5:SetTarget(c270772000.sptg)
-	e5:SetOperation(c270772000.spop)
+	e5:SetCondition(c210772000.spcon)
+	e5:SetTarget(c210772000.sptg)
+	e5:SetOperation(c210772000.spop)
 	e5:SetLabelObject(e4)
 	c:RegisterEffect(e5)
 end
 
-function c270772000.damcon(e,tp,eg,ep,ev,re,r,rp)
+function c210772000.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler()==Duel.GetAttackTarget()
 end
 
-function c270772000.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c210772000.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAttackPos() end
 	Duel.SetTargetPlayer(1-tp)
 	local atk=Duel.GetAttacker():GetAttack()
 	Duel.SetTargetParam(atk)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
 end
-function c270772000.damop(e,tp,eg,ep,ev,re,r,rp)
+function c210772000.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
 
-function c270772000.mtfilter(c)
+function c210772000.mtfilter(c)
 	return c:IsSetCard(0xa4) and c:IsDiscardable()
 end
 
-function c270772000.mtcon(e,tp,eg,ep,ev,re,r,rp)
+function c210772000.mtcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
 
-function c270772000.mtop(e,tp,eg,ep,ev,re,r,rp)
+function c210772000.mtop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 and Group.IsExists(g,c270772000.mtfilter, 1,nil) and Duel.SelectYesNo(tp,aux.Stringid(270772000,2)) then
-		Duel.DiscardHand(tp,c270772000.mtfilter,1,1,REASON_COST+REASON_DISCARD)
+	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 and Group.IsExists(g,c210772000.mtfilter, 1,nil) and Duel.SelectYesNo(tp,aux.Stringid(270772000,2)) then
+		Duel.DiscardHand(tp,c210772000.mtfilter,1,1,REASON_COST+REASON_DISCARD)
 	else
 		Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 	end
 end
 
-function c270772000.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c210772000.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_EFFECT) and not c:IsLocation(LOCATION_DECK)
 		and c:GetPreviousControler()==tp and c:IsPreviousPosition(POS_FACEUP) and c:GetReasonPlayer()==1-tp
 end
 
-function c270772000.spfilter(c,e,tp)
+function c210772000.spfilter(c,e,tp)
 	return c:IsSetCard(0xa4) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
 
-function c270772000.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c210772000.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c270772000.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c210772000.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 
-function c270772000.spop(e,tp,eg,ep,ev,re,r,rp)
+function c210772000.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c270772000.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c210772000.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
 		Duel.ShuffleDeck(tp)
