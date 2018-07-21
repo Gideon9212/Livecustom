b3246649a091871447aa85cdb6de347eecadc50d
EerieCode <eeriecode@gmail.com>
2018-07-21 09:45:03 +0200

    ID updates
    "Supreme King Dragon Rebelwurm" moved to its original creator's range
    
    "Dimension Synchro" renamed to "Hyper-Dimension Synchro", ID changed and image updated for consistency with future members of the series

--- script/c210220078.lua
+++ script/c210220078.lua
@@ -1,24 +1,25 @@
---ディメンション・シンクロ
+--HDi・シンクロ
 --Dimension Synchro
 --Created and scripted by Eerie Code
-function c210220070.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,210220070+EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(c210220070.cost)
-	e1:SetTarget(c210220070.target)
-	e1:SetOperation(c210220070.activate)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function c210220070.matfilter(c)
+function s.matfilter(c)
 	return c:IsCanBeSynchroMaterial()
 		and (c:IsFaceup() or not c:IsLocation(LOCATION_ONFIELD))
 		and (c:IsPreviousLocation(LOCATION_ONFIELD) or not c:IsLocation(LOCATION_GRAVE))
 end
-function c210220070.filter(c,e,tp,mg)
+function s.filter(c,e,tp,mg)
 	if not c:IsType(TYPE_SYNCHRO) then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -29,22 +30,22 @@ function c210220070.filter(c,e,tp,mg)
 	e1:Reset()
 	return res
 end
-function c210220070.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local mg=Duel.GetMatchingGroup(c210220070.matfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,nil)
 	if chk==0 then return Duel.CheckLPCost(tp,1500)
-		and Duel.IsExistingMatchingCard(c210220070.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg) end
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg) end
 	Duel.PayLPCost(tp,1500)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectMatchingCard(tp,c210220070.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,mg)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,mg)
 	Duel.ConfirmCards(1-tp,g)
 	e:SetLabelObject(g:GetFirst())
 end
-function c210220070.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c210220070.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetMatchingGroup(c210220070.matfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE,0,nil)
 	local tc=e:GetLabelObject()
 	if not tc:IsLocation(LOCATION_EXTRA) then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
