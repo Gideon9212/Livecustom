7cc7a827b459ccc54e86b368b5f7b0325e6ae4de
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-14 15:47:56 -0300

    Update c210777059.lua

--- script/c210777059.lua
+++ script/c210777059.lua
@@ -56,7 +56,7 @@ function c210777059.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xf10) and not c:IsCode(210777059)
 end
 function c210777059.drcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(c210777059.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function c210777059.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
