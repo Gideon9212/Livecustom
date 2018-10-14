0db49aa6a7e6b989b30b82dafd385d6b62c47d30
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-14 11:05:57 -0300

    Reverted ID usage to old way for "The Beast That Lurks" archetype
    Removed the usage of GetID, that didn't work in the server and apply the old way to handle IDs

--- script/c210777061.lua
+++ script/c210777061.lua
@@ -1,16 +1,16 @@
 --The Beast That Lurks in the Rays
---designed by Thaumablazer#4134, scripte by Naim
-local s,id=GetID()
-function s.initial_effect(c)
+--designed by Thaumablazer#4134, scripted by Naim
+--local s,id=GetID()
+function c210777061.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
-	e1:SetOperation(s.spop)
+	e1:SetCountLimit(1,210777061)
+	e1:SetCondition(c210777061.spcon)
+	e1:SetOperation(c210777061.spop)
 	c:RegisterEffect(e1)
 	--immune
 	local e2=Effect.CreateEffect(c)
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_IMMUNE_EFFECT)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(s.efilter)
+	e2:SetValue(c210777061.efilter)
 	c:RegisterEffect(e2)
 	--gy effect (mill deck)
 	local e3=Effect.CreateEffect(c)
@@ -26,25 +26,25 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCountLimit(1,id+100)
+	e3:SetCountLimit(1,210777061+100)
 	e3:SetCost(aux.bfgcost)
-	e3:SetCondition(s.drcond)
-	e3:SetTarget(s.drtg)
-	e3:SetOperation(s.drop)
+	e3:SetCondition(c210777061.drcond)
+	e3:SetTarget(c210777061.drtg)
+	e3:SetOperation(c210777061.drop)
 	c:RegisterEffect(e3)
 end
-function s.spcon(e,c)
+function c210777061.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_HAND,0,1,c)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function c210777061.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
 	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,1,1,c)
 	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
 end
-function s.efilter(e,re)
+function c210777061.efilter(e,re)
 	local cg=e:GetHandler():GetColumnGroup()
 	local typecheck=0
 	for tc in aux.Next(cg) do
@@ -52,22 +52,22 @@ function s.efilter(e,re)
 	end
 	return typecheck~=0 and re:IsActiveType(typecheck)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xf10) and not c:IsCode(id)
+function c210777061.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xf10) and not c:IsCode(210777061)
 end
-function s.drcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+function c210777061.drcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c210777061.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function s.tgfilter(c)
+function c210777061.tgfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+function c210777061.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(c210777061.tgfilter,tp,LOCATION_DECK,0,1,nil) end
   Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
+function c210777061.drop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c210777061.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
--- script/c210777062.lua
+++ script/c210777062.lua
@@ -1,16 +1,16 @@
 --The Beast That Lurks in the Clouds
 --designed by Thaumablazer#4134, scripted by Naim
-local s,id=GetID()
-function s.initial_effect(c)
+--local s,id=GetID()
+function c210777062.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
-	e1:SetOperation(s.spop)
+	e1:SetCountLimit(1,210777062)
+	e1:SetCondition(c210777062.spcon)
+	e1:SetOperation(c210777062.spop)
 	c:RegisterEffect(e1)
 	--immune
 	local e2=Effect.CreateEffect(c)
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_IMMUNE_EFFECT)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(s.efilter)
+	e2:SetValue(c210777062.efilter)
 	c:RegisterEffect(e2)
 	--gy effect (shuffle s/t)
 	local e3=Effect.CreateEffect(c)
@@ -26,25 +26,25 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCountLimit(1,id+100)
+	e3:SetCountLimit(1,210777062+100)
 	e3:SetCost(aux.bfgcost)
-	e3:SetCondition(s.tdcond)
-	e3:SetTarget(s.tdtg)
-	e3:SetOperation(s.tdop)
+	e3:SetCondition(c210777062.tdcond)
+	e3:SetTarget(c210777062.tdtg)
+	e3:SetOperation(c210777062.tdop)
 	c:RegisterEffect(e3)
 end
-function s.spcon(e,c)
+function c210777062.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_HAND,0,1,c)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function c210777062.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
 	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,1,1,c)
 	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
 end
-function s.efilter(e,re)
+function c210777062.efilter(e,re)
 	local cg=e:GetHandler():GetColumnGroup()
 	local typecheck=0
 	for tc in aux.Next(cg) do
@@ -52,22 +52,22 @@ function s.efilter(e,re)
 	end
 	return typecheck~=0 and re:IsActiveType(typecheck)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xf10) and not c:IsCode(id)
+function c210777062.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xf10) and not c:IsCode(210777062)
 end
-function s.tdcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+function c210777062.tdcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c210777062.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function s.desfilter(c)
+function c210777062.desfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeck()
 end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil) end
+function c210777062.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(c210777062.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c210777062.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
 end
-function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+function c210777062.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
